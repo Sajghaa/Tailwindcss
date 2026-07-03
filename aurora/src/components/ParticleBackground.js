720a6c437940f8cdcd773edd7af1aa84908e565a
Serge <sajghaa@gmail.com>
2026-07-03 21:53:00 +0200

    Updated ParticleBackground.js

---
+++ aurora/src/components/ParticleBackground.js
@@ -0,0 +1,4 @@
+export function ParticleBackground() {
+  // Canvas element with ID – the main.js will attach the particle logic to it
+  return `<canvas id="particleCanvas" class="fixed inset-0 -z-10 pointer-events-none"></canvas>`;
+}
