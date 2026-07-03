290328913d23dde108cbb119b079ca3098e37b0f
Serge <sajghaa@gmail.com>
2026-07-03 21:43:13 +0200

    Updated vite.config.js

---
+++ aurora/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: { open: true },
+});
